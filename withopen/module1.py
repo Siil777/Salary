68280a2e6255328badba5330d3cd1cfa984de16c
Siil777 <118188578+Siil777@users.noreply.github.com>
2023-02-06 14:36:43 +0200

    Update module1.py

--- withopen/module1.py
+++ withopen/module1.py
@@ -1,6 +1,3 @@
-
-
-
 def read_file(file:str)->list:
     """
     loeme failist
@@ -59,6 +56,10 @@ def delete(nimi:str,p:list,i:list):
 
     #return mas
 def MAX(p:list,i:list):
+     """ max calculates max value in the list
+    :param p:list
+    :param i:list
+    """
     p=list(map(int,p))
     #for palk in p:
     max_palk=max(p)
@@ -70,6 +71,123 @@ def MAX(p:list,i:list):
         nimi=i[ind]
         print(f'{nimi}')
         pos=ind+1
+def MIN(p:list,i:list):
+    p=list(map(int,p))
+    min_palk=min(p)
+    n=p.count(min_palk)
+    pos=0
+    print(f'min salary {min_palk}\n')
+    for j in range(n):
+        ind=p.index(min_palk,pos)
+        nimi=i[ind]
+        print(f'{nimi}')
+        pos=ind+1
+
+def iqual_salary(p:list,i:list):
+     """min calculates min value in the list
+    :param p:list
+    :param i:list
+    """
+    palgad=read_file('Palgad_file.txt')
+    inimised=read_file('inimised_file.txt')
+    list1=[] 
+    list2=[] 
+    N=len(palgad) 
+    for h in range(N+1):
+        for j in range(h+1,N):
+            if palgad[h]==palgad[j]:
+                a=h
+    b=palgad[a] 
+    print(b) 
+    for h in range(len(palgad)): 
+        if b==palgad[h]: 
+            list1.append(h)
+    for h in range(len(list1)): 
+        list2.append(inimised[list1[h]]) 
+    print(f'repeat slary',b,h,'times')
+
+def increase_waning(p:list,i:list): 
+    """
+    :param p:list
+    :param i:list
+    """
+    p=list(map(int,p))
+    i=list(map(str,i)) 
+    print('rise')
+    p.sort() 
+    i.sort()
+    print(p) 
+    print(i)
+    print('descending')
+    p.sort(reverse=True) 
+    i.sort(reverse=True) 
+    print(p) 
+    print(i)
+ 
+    
+    
+       
+        
+       
+   
+
+
+def name_salary(p:list,i:list):
+    """
+    :param p:list
+    :param i:list
+    """
+    p=list(map(int,p)) 
+    pos=0 
+    n=input('name:')
+    ind=i.index(n,pos)  
+    n=print(p[ind]) 
+    pos=ind+1
+
+def top_salary(p:list, i:list):
+    """
+    :param p:list
+    :param i:list
+    """
+   
+    p=list(map(int, p))
+    l = p.copy()
+    v = i.copy()
+
+    vv = []
+    vd =[]
+
+    for j in range(0, 3):   
+        y = p.index(max(p))
+        v1 = l.index(min(l))
+        vv.append(i[y])
+        vd.append(v[v1])
+        p.remove(max(p))
+        l.remove(min(l))
+        i.remove(i[y])
+        v.remove(v[v1])
+    print('top of rich workers:')
+    for j in vv:
+        print(j)
+    print('top of poor workers:')
+    for j in vd:
+        print(j)
+    return vv, vd
+
+def average(p:list,i:list): 
+    """
+    :param p:list
+    :param i:list
+    """
+    p=list(map(int,p)) 
+    avg=sum(p)/len(p) 
+    avg=int(avg)  
+    if avg in p: 
+        v=p.index(avg)  
+        print(f'{avg} {i[v]}') 
+    else: 
+        print(f'average salary is {avg}')  
+    return avg
 
 
 
